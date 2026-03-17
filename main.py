6ac0ef9602f4a71b2692d7293b78b31a994ab3f2
ABdroid926 <aadhirai.b@babajividhyashram.org>
2026-03-17 20:05:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-
-
-
+api_key = st.secrets["GOOGLE_API_KEY"]
 st.set_page_config(page_title="Whats For Dinner", page_icon="🍳")
 st.title('Whats For Dinner 🍳 || Recipe Reccommender ')
 
@@ -24,7 +22,7 @@ def generate_recommendations(input_text):
     try:
         llm = ChatGoogleGenerativeAI(
             model="gemini-2.5-flash",
-            google_api_key=st.secrets["GOOGLE_API_KEY"]
+            google_api_key=api_key
             temperature=0.7
         )
         prompt = f"Given the ingredients: {input_text}, suggest five easy-to-cook step-by-step recipes."
